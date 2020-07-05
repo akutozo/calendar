b3774b23a13ef2524a92ae1a0a8cd17563c683ac
Jacob Daughtry <akutozo@gmail.com>
2020-07-05 16:07:37 -0500

    Clean Up

--- assets/javascript/mochajava.js
+++ assets/javascript/mochajava.js
@@ -5,11 +5,3 @@ $today.text(now);
 
 //Let's store some information
 // let dailyAgenda = JSON.parse(localStorage.getItem("dailyAgenda"));
-
-
-$('input').on('input', function () {
-    console.log($(this).val());
-    if ($(this).val() === "") {
-        localStorage.setItem($(this).attr("id"), "");
-    }
-}) 
