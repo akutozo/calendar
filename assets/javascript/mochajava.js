db7cf7aa95e63a03d1fbb0e9144513a3b9e0f8ee
Jacob Daughtry <akutozo@gmail.com>
2020-07-05 16:06:11 -0500

    HTML corrections

--- assets/javascript/mochajava.js
+++ assets/javascript/mochajava.js
@@ -6,3 +6,10 @@ $today.text(now);
 //Let's store some information
 // let dailyAgenda = JSON.parse(localStorage.getItem("dailyAgenda"));
 
+
+$('input').on('input', function () {
+    console.log($(this).val());
+    if ($(this).val() === "") {
+        localStorage.setItem($(this).attr("id"), "");
+    }
+}) 
